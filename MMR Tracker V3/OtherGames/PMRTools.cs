2b5874bd3069bbe0e2fb0bf62139df8596624f48
thedrummonger <drummerxtmail@gmail.com>
2022-04-12 10:28:35 -0500

    Initial Files for Paper Mario Rando Support

---
+++ MMR Tracker V3/OtherGames/PMRTools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMR_Tracker_V3.OtherGames
+{
+    class PMRTools
+    {
+        class PaperMarioLogicJSON
+        {
+            public PMRArea from;
+            public PMRArea to;
+            public List<Tuple<string, string>> reqs;
+            public List<string> pseudoitems;
+        }
+        class PMRArea
+        {
+            public string map;
+            public dynamic id;
+        }
+
+        public static void ReadLogicJson()
+        {
+
+        }
+    }
+}
