5efe1e2f1ba402e9c67a4df0b8170272bfdf1192
thedrummonger <drummerxtmail@gmail.com>
2023-02-17 15:15:11 -0600

    Maybe fixed issue building on android? Maui is weird man.

---
+++ WorkAround10150.bat
@@ -0,0 +1,2 @@
+xcopy ..\MauiFrontEnd\bin\Debug\net6.0\*.* ..\MauiFrontEnd\bin\Debug\net6.0\android-x64\*.* /Y
+xcopy "..\MMR Tracker V3\bin\Debug\net6.0-android\*.*" "..\MMR Tracker V3\bin\Debug\net6.0-android\android-x64\*.*" /Y
