de5239c3de0e6f1c1ff0d1ec68ea4c756f9a04ec
Thedrummonger <drummerxtmail@gmail.com>
2023-02-18 08:35:30 -0600

    Actually fixed the android builds. I think this bug is inconsistent... Also made the fix work for 32-bit android OS.

--- WorkAround10150.bat
+++ WorkAround10150.bat
@@ -1,2 +1,4 @@
 xcopy ..\MauiFrontEnd\bin\Debug\net6.0\*.* ..\MauiFrontEnd\bin\Debug\net6.0\android-x64\*.* /Y
 xcopy "..\MMR Tracker V3\bin\Debug\net6.0-android\*.*" "..\MMR Tracker V3\bin\Debug\net6.0-android\android-x64\*.*" /Y
+xcopy ..\MauiFrontEnd\bin\Debug\net6.0\*.* ..\MauiFrontEnd\bin\Debug\net6.0\android-x86\*.* /Y
+xcopy "..\MMR Tracker V3\bin\Debug\net6.0-android\*.*" "..\MMR Tracker V3\bin\Debug\net6.0-android\android-x86\*.*" /Y
